3222348c465bfe85edcee095f4a87b939dc789a2
Angelo92Git <52296518+Angelo92Git@users.noreply.github.com>
2023-07-23 10:16:12 -0400

    Add CV and use the best lags from CV, add plotting

--- AR_forecast.py
+++ AR_forecast.py
@@ -13,33 +13,147 @@ import my_utils as mu
 with open('validated_data.pkl', 'rb') as f:
     df = pkl.load(f)
 
-my_mod = mu.fit_AR_model(df, 310, 'x', lags=35)
-mu.plot_AR_forecast(df, 310, 'x', my_mod)
+# my_mod = mu.fit_AR_model(df, 310, 'x', lags=35)
+# mu.plot_AR_forecast(df, 310, 'x', my_mod)
 
-# Declare tracking variables
+cross_validate = False
+if cross_validate:
+#region Cross-validation
+    #----------------------------------------------------------------------------------------
+    # Cross-validation
+    #----------------------------------------------------------------------------------------
+    # Assume that the same lag should be used for all variables
+    # id = 598 #DEBUG Remove
+    # df = df.query(f'sat_id >= {id}')#DEBUG Remove
+    def CV(df, cv_func, num_fits=4, num_lags=50):
+        best_smape_cv_scores = np.full(len(df['sat_id'].unique()), -np.inf)
+        best_vec_smape_cv_scores = np.full(len(df['sat_id'].unique()), -np.inf)
+        smape_best_lags = np.full(len(df['sat_id'].unique()), np.nan)
+        vec_smape_best_lags = np.full(len(df['sat_id'].unique()), np.nan)
+        for sat_id, sat_group in tqdm(df.groupby('sat_id'), position=0, leave=True, desc='Cross-validation'):
+            # sat_id = sat_id - id #DEBUG Remove
+            sat_group = mu.set_epoch_as_index_and_freq(sat_group)
+            train = sat_group.query('is_train == True')
+            for lags in tqdm(np.arange(1, num_lags+1), position=1, leave=False, desc = 'Lags'):
+                cv_smape_accuracy = []
+                all_vec_targets = []
+                all_vec_forecasts = []
+                for cv_train, cv_val in cv_func(train, num_fits=num_fits):
+                    if len(cv_train)-lags <= lags+2:
+                        continue
+                    all_vec_targets.append(cv_val[['x', 'y', 'z', 'Vx', 'Vy', 'Vz']].to_numpy().reshape(-1,6)) 
+                    var_vec_forecasts = []
+                    for var in tqdm(['x', 'y', 'z', 'Vx', 'Vy', 'Vz'], position=2, leave=False, desc = 'Variables'):
+                        cv_model = AutoReg(cv_train[var], lags=lags, trend = 'ct')
+                        cv_model_fit = cv_model.fit()
+                        cv_forecast = cv_model_fit.forecast(steps=len(cv_val))
+                        cv_smape_accuracy.append(mu.get_percent_accuracy(cv_val[var], cv_forecast, mm.smape))
+
+                        var_vec_forecasts.append(cv_forecast.to_numpy().reshape(-1,1))
+
+                    fold_vec_forecasts = np.concatenate(var_vec_forecasts, axis=1)
+                    all_vec_forecasts.append(fold_vec_forecasts)
+
+                if not cv_smape_accuracy or not all_vec_targets: # Check if empty
+                    continue
+
+                cv_smape_accuracy_lag = np.mean(cv_smape_accuracy)
+                if  cv_smape_accuracy_lag > best_smape_cv_scores[sat_id]:
+                    best_smape_cv_scores[sat_id] = cv_smape_accuracy_lag
+                    smape_best_lags[sat_id] = lags
+
+                all_vec_targets = np.concatenate(all_vec_targets, axis=0)
+                all_vec_forecasts = np.concatenate(all_vec_forecasts, axis=0)
+                cv_vec_smape_accuracy_lag = mu.get_percent_accuracy(all_vec_targets, all_vec_forecasts, mm.vector_smape)
+                if cv_vec_smape_accuracy_lag > best_vec_smape_cv_scores[sat_id]:
+                    best_vec_smape_cv_scores[sat_id] = cv_vec_smape_accuracy_lag
+                    vec_smape_best_lags[sat_id] = lags   
+
+        return smape_best_lags, vec_smape_best_lags
+
+
+    ecv_smape_best_lags, ecv_vec_smape_best_lags = CV(df, mu.expanding_window_cv)
+    # Save ECV
+    with open('Results/expanding_window_smape_best_lags.pkl', 'wb') as f:
+        pkl.dump(ecv_smape_best_lags, f)
+
+    with open('Results/expanding_window_vec_smape_best_lags.pkl', 'wb') as f:
+        pkl.dump(ecv_vec_smape_best_lags, f)
+
+    scv_smape_best_lags, scv_vec_smape_best_lags = CV(df, mu.sliding_window_cv)
+    # Save SCV
+
+    with open('Results/sliding_window_smape_best_lags.pkl', 'wb') as f:
+        pkl.dump(scv_smape_best_lags, f)
+
+    with open('Results/sliding_window_vec_smape_best_lags.pkl', 'wb') as f:
+        pkl.dump(scv_vec_smape_best_lags, f)
+#endregion
+
+with open('Results/expanding_window_smape_best_lags.pkl', 'rb') as f:
+    ecv_vec_smape_best_lags = pkl.load(f)
+
+# #--------------------------------------------------------------------------------------------------
+# # Declare tracking variables
+# #--------------------------------------------------------------------------------------------------
 var_index = {'x': 0, 'y': 1, 'z': 2, 'Vx': 3, 'Vy': 4, 'Vz': 5}
-accuracy_dict = {}
-accuracy_tensor = np.full((len(df['sat_id'].unique()), 6), np.nan)
+vars = ['x', 'y', 'z', 'Vx', 'Vy', 'Vz']
+# SMAPE scores directly from AR model forecast
+smape_accuracy_dict = {} # Used for easy readability
 
+# dim 600 x 6
+smape_accuracy_tensor = np.full((len(df['sat_id'].unique()), 6), np.nan) # Used for exploration, 6 univariate timeseries
+
+# Vector SMAPE scores from AR model forecast
+# dim 600 x 1
+vec_smape_accuracy_tensor = np.full((len(df['sat_id'].unique()), 1), np.nan) #  Used for exploration, 1 multivariate timeseries
+# Store variable forecasts in a dictionary for vector SMAPE
+var_forecasts_dict = {}
+
+#--------------------------------------------------------------------------------------------------
+# Iterate over satellites and forecast
+#--------------------------------------------------------------------------------------------------
 for sat_id, sat_group in tqdm(df.groupby('sat_id'), position=0, leave=True, desc = 'Satellites'):
+    # Prepare data and set store keys
     sat_group = mu.set_epoch_as_index_and_freq(sat_group)
-    accuracy_dict[sat_id] = {}
+    smape_accuracy_dict[sat_id] = {}
+    var_forecasts_dict[sat_id] = {}
+    best_lag = ecv_vec_smape_best_lags[sat_id]
+    if best_lag < 35:
+        best_lag = 35 # Due to the scarcity of data for some satellites, we weren't able to test lags > 35 for all satellites
+        # When this happens, we use the maximum lag that was tested for all satellites 
 
     train = sat_group.query('is_train == True')
     test = sat_group.query('is_train == False')
     for var in tqdm(['x', 'y', 'z', 'Vx', 'Vy', 'Vz'], position=1, leave=False, desc = 'Variables'):
-        model = AutoReg(train[var], lags=35, trend = 'ct')
+        model = AutoReg(train[var], lags=best_lag, trend = 'ct')
         model_fit = model.fit()
         forecast = model_fit.forecast(steps=len(test))
+        # Store forecasts for vector SMAPE
+        var_forecasts_dict[sat_id][var] = forecast
 
-        accuracy = 100*(1 - mm.smape(test[var].to_numpy(), forecast.to_numpy()))
-        accuracy_dict[sat_id][var] = accuracy
-        accuracy_tensor[sat_id][var_index[var]] = accuracy
-    assert (not np.any(np.isnan(accuracy_tensor[sat_id,:]))), f"NaNs in accuracy_tensor for sat_id {sat_id}"
+        # Calculate SMAPE accuracy per satellite and per variable and store in dictionary and tensor
+        smape_accuracy = mu.get_percent_accuracy(test[var], forecast, mm.smape)
+        smape_accuracy_dict[sat_id][var] = smape_accuracy
+        smape_accuracy_tensor[sat_id][var_index[var]] = smape_accuracy
+    assert (not np.any(np.isnan(smape_accuracy_tensor[sat_id,:]))), f"NaNs in smape_accuracy_tensor for sat_id {sat_id}"
 
-assert (not np.any(np.isnan(accuracy_tensor))), f"NaNs in accuracy_tensor"
+    forecasts_array = np.array([var_forecasts_dict[sat_id][var] for var in vars]).T
+    vec_smape_accuracy_tensor[sat_id] = mu.get_percent_accuracy(test[vars].to_numpy(), forecasts_array, mm.vector_smape)
+    assert (not np.isnan(vec_smape_accuracy_tensor[sat_id])), f"NaNs in vec_smape_accuracy_tensor for sat_id {sat_id}"
 
-#TODO vec_accuracy
-#TODO: Cross-validation
-#TODO: VAR_model
-#TODO: DeepAR_model
+# Check for NaNs and negative values
+assert (not np.any(np.isnan(smape_accuracy_tensor))), f"NaNs in smape_accuracy_tensor"
+assert (not np.any(np.isnan(vec_smape_accuracy_tensor))), f"NaNs in vec_smape_accuracy_tensor"
+assert (np.any(smape_accuracy_tensor >= 0)), f"Negative values in smape_accuracy_tensor"
+assert (np.any(vec_smape_accuracy_tensor >= 0)), f"Negative values in vec_smape_accuracy_tensor"
+
+# Save results
+with open('Results/AR_ct_accuracy_best_lags.pkl', 'wb') as f:
+    pkl.dump(smape_accuracy_tensor, f)
+
+with open('Results/AR_ct_vec_accuracy_best_lags.pkl', 'wb') as f:
+    pkl.dump(vec_smape_accuracy_tensor, f)
+
+# TODO: VAR_model
+# TODO: DeepAR_model
--- my_utils.py
+++ my_utils.py
@@ -2,7 +2,10 @@ import pandas as pd
 import statsmodels.api as sm
 from statsmodels.tsa.ar_model import AutoReg
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 import numpy as np
+import my_metrics as mm
+import pickle as pkl
 
 def set_epoch_as_index_and_freq(df: pd.DataFrame) -> pd.DataFrame:
     assert df['epoch'].diff()[1:].unique().shape == (1,), f"intervals not consistent for sat_id {df['sat_id'][0]}"
@@ -20,8 +23,25 @@ def fit_AR_model(df, sat_id, var, lags=35, trend='ct'):
     return model_fit
 
 def get_percent_accuracy(target, forecast, metric_func):
-    accuracy = 100*(1 - metric_func(target.to_numpy(), forecast.to_numpy()))
+    accuracy = 100*(1 - metric_func(target, forecast))
     return accuracy
+
+def expanding_window_cv(train, num_fits=4):
+    foldsize = len(train) // (num_fits+1) # The first fold is for training only, the rest are for validation as the folds get subsumed unto training at each step
+    horizon = foldsize
+    for i in range(num_fits):
+        split_train = train.iloc[:(i+1)*foldsize]
+        split_val = train.iloc[(i+1)*foldsize:(i+1)*foldsize+horizon]
+        yield (split_train, split_val)
+
+def sliding_window_cv(train, num_fits=4):
+    foldsize = len(train) // num_fits
+    windowsize = (foldsize*3//4) # training window
+    horizon = foldsize - windowsize # validation window
+    for i in range(num_fits):
+        split_train = train.iloc[i*foldsize:i*foldsize+windowsize]
+        split_val = train.iloc[i*foldsize+windowsize:i*foldsize+windowsize+horizon]
+        yield (split_train, split_val)
 
 def plot_AR_forecast(df, sat_id, var, model_fit):
     fig = plt.figure()
@@ -51,3 +71,64 @@ def plot_AR_forecast(df, sat_id, var, model_fit):
     plt.tight_layout()
     plt.show()
     return
+
+def plot_CV(df, sat_id, cv_func, num_fits=4, num_lags=50):
+    sat_group = df.query(f'sat_id=={sat_id}')
+    sat_group = set_epoch_as_index_and_freq(sat_group)
+    train = sat_group.query('is_train == True')
+    cv_smape_accuracy_lag = []
+    cv_vec_smape_accuracy_lag = []
+    lags_used = []
+    for lags in tqdm(np.arange(1, num_lags+1), position=1, leave=False, desc = 'Lags'):
+        cv_smape_accuracy = []
+        all_vec_targets = []
+        all_vec_forecasts = []
+        for cv_train, cv_val in cv_func(train, num_fits=num_fits):
+            if len(cv_train)-lags <= lags+2:
+                continue
+            all_vec_targets.append(cv_val[['x', 'y', 'z', 'Vx', 'Vy', 'Vz']].to_numpy().reshape(-1,6)) 
+            var_vec_forecasts = []
+            for var in tqdm(['x', 'y', 'z', 'Vx', 'Vy', 'Vz'], position=2, leave=False, desc = 'Variables'):
+                cv_model = AutoReg(cv_train[var], lags=lags, trend = 'ct')
+                cv_model_fit = cv_model.fit()
+                cv_forecast = cv_model_fit.forecast(steps=len(cv_val))
+                cv_smape_accuracy.append(mm.smape(cv_val[var], cv_forecast))
+
+                var_vec_forecasts.append(cv_forecast.to_numpy().reshape(-1,1))
+
+            fold_vec_forecasts = np.concatenate(var_vec_forecasts, axis=1)
+            all_vec_forecasts.append(fold_vec_forecasts)
+        
+        if not cv_smape_accuracy or not all_vec_targets: # Check if empty
+            continue
+        
+        lags_used.append(lags)
+        cv_smape_accuracy_lag.append(np.mean(cv_smape_accuracy))
+        
+        all_vec_targets = np.concatenate(all_vec_targets, axis=0)
+        all_vec_forecasts = np.concatenate(all_vec_forecasts, axis=0)
+        cv_vec_smape_accuracy_lag.append(mm.vector_smape(all_vec_targets, all_vec_forecasts))
+    
+    x_plot = lags_used
+    y1_plot = cv_smape_accuracy_lag
+    y2_plot = cv_vec_smape_accuracy_lag
+    best_lag = np.argmin(y1_plot)+1
+    plt.plot(x_plot, y1_plot, label='SMAPE')
+    plt.plot(x_plot, y2_plot, label='Vector SMAPE')
+    plt.xlabel('Lags')
+    plt.ylabel('CV score')
+    plt.title(f'Cross-validation score for sat_id {sat_id}')
+    plt.legend(loc='upper right')
+    plt.xlim(1, np.max(lags_used))
+    plt.ylim(0, 1)
+    plt.plot([best_lag, best_lag],[0 , 1], color = 'r')
+    plt.tight_layout()
+    plt.savefig(f'Results/CV_accuracy_{sat_id}.png')
+    plt.close('all')
+    return
+
+
+# with open('validated_data.pkl', 'rb') as f:
+#     df = pkl.load(f)
+
+# plot_CV(df, 310, sliding_window_cv)
